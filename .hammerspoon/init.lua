60d3280b9c373a116a9ab8fc65aebf0316e13f6e
Anton Tsviatkou <a.tsviatkou@gmail.com>
2016-09-01 22:06:33 -0700

    Add caffeine icon into menu

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -13,6 +13,31 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
     reloadPlusAlert()
 end)
 
+-- menu icon to access Mac's caffeine functions
+local caffeine = hs.menubar.new()
+
+function setCaffeineDisplay(state)
+    local result
+    if state then
+        result = caffeine:setIcon("assets/stay-awake.pdf")
+        caffeine:setTooltip("Caffeine is on - Mac won't sleep")
+    else
+        result = caffeine:setIcon("assets/can-sleep.pdf")
+        caffeine:setTooltip("Caffeine is off - Mac is allowed to sleep")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    -- enable caffeine and prevent sleep by default
+    hs.caffeinate.set('displayIdle', true, true)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
 -- watch & reload config automatically if changed
 function reloadConfig(files)
     doReload = false
@@ -26,3 +51,4 @@ function reloadConfig(files)
     end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
