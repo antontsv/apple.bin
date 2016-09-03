33842bda206f14f753daaa3245ed4472a34d3143
Anton Tsviatkou <a.tsviatkou@gmail.com>
2016-09-02 23:19:01 -0700

    Add window maximize key combination

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,4 +1,9 @@
 
+-- shift (⇧ ); control/crtl (⌃); option/alt (⌥ ); command/cmd (⌘ )
+local scod = {"shift","ctrl","alt","cmd"}
+local cd = {"ctrl","cmd"}
+local cod = {"ctrl","alt","cmd"}
+
 -- redshift to 2800K from 20:30->21:30
 -- and back to standard 6:30->7:30
 hs.redshift.start(2800,'21:00','7:00','1h')
@@ -9,9 +14,10 @@ function reloadPlusAlert()
 end
 
 --config reloading. manual (from getting started guide):
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+hs.hotkey.bind(cod, "R", function()
     reloadPlusAlert()
 end)
+
 
 -- menu icon to access Mac's caffeine functions
 local caffeine = hs.menubar.new()
@@ -52,3 +58,20 @@ function reloadConfig(files)
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
+
+-- Windows
+-- Toggle a window regular frame and maximized on the screen
+local savedFrameSizes = {}
+function toggle_maximize()
+    local win = hs.window.focusedWindow()
+    if savedFrameSizes[win:id()] then
+        win:setFrame(savedFrameSizes[win:id()])
+        savedFrameSizes[win:id()] = nil
+    else
+        savedFrameSizes[win:id()] = win:frame()
+        win:maximize()
+    end
+end
+-- Control + Option + Command + F to maximize (different from full screen)
+-- Note: Mac's Control + Command + F triggers app full-screen mode
+hs.hotkey.bind(cod, 'f', toggle_maximize)
